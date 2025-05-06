d4081a052ed5a9605725a2035a18e6930e3639f3
Brondoz97 <129960536+Brondoz@users.noreply.github.com>
2025-05-06 17:37:18 +0200

    Initial commit Vue.js frontend

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="section">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <div class="box">
+            <h1 class="title has-text-centered">Connexion</h1>
+
+            <form @submit.prevent="handleSubmit" @reset="handleReset">
+              <!-- Email -->
+              <div class="field">
+                <label class="label" for="email">Email</label>
+                <div class="control has-icons-left">
+                  <input
+                    class="input is-info"
+                    type="email"
+                    id="email"
+                    placeholder="Entrez votre email"
+                    v-model="email"
+                    required
+                  />
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-envelope"></i>
+                  </span>
+                </div>
+              </div>
+
+              <!-- Password -->
+              <div class="field">
+                <label class="label" for="password">Mot de passe</label>
+                <div class="control has-icons-left">
+                  <input
+                    class="input is-info"
+                    type="password"
+                    id="password"
+                    placeholder="Entrez votre mot de passe"
+                    v-model="password"
+                    required
+                  />
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                  </span>
+                </div>
+              </div>
+
+              <!-- Buttons -->
+              <div class="field is-grouped is-grouped-right">
+                <p class="control">
+                  <button class="button is-info" type="submit">Connexion</button>
+                </p>
+                <p class="control">
+                  <button class="button is-danger" type="reset">Réinitialiser</button>
+                </p>
+              </div>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const email = ref('')
+const password = ref('')
+
+const handleSubmit = () => {
+  if (!email.value || !password.value) {
+    alert('Veuillez remplir tous les champs.')
+  } else {
+    console.log('Email :', email.value)
+    console.log('Password :', password.value)
+    alert('Connexion envoyée !')
   }
 }
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+const handleReset = () => {
+  email.value = ''
+  password.value = ''
 }
-</style>
+</script>
